60e28ce1830d1ed39ce8046b7ba51ba1f2a03970
ShadowShark99 <kadenwchan@gmail.com>
2025-06-23 18:43:47 -0500

    remove fail test

--- sum.test.js
+++ sum.test.js
@@ -4,10 +4,6 @@ test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("adds 4 + 5 to equal 10", () => {
-  expect(sum(4, 5)).toBe(10);
-});
-
 test("adds poop + pee to equal pooppee", () => {
   expect(sum("poop", "pee")).toBe("pooppee");
 });
