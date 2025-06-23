6ca3be6926406ff80fcc74290c604199efe6a2e2
ShadowShark99 <kadenwchan@gmail.com>
2025-06-23 18:30:57 -0500

    Write Caesar tests

---
+++ caesar.test.js
@@ -0,0 +1,13 @@
+const caesar = require("./caesar");
+
+test("caesar banana -> 3", () => {
+  expect(caesar("banana", 3)).toBe("edqdqd");
+});
+
+test("caesar abc -> 5", () => {
+  expect(caesar("abc", 5)).toBe("fgh");
+});
+
+test("caesar @#$%coolPASSWORD -> 1", () => {
+  expect(caesar("@#$%coolPASSWORD#$%", 1)).toBe("%$#@#$%dppmQBTTXPSE");
+});
