c5918db8fba3e2e3ff7142f387b8ac7c10f677b9
ShadowShark99 <kadenwchan@gmail.com>
2025-06-23 19:22:36 -0500

    Use toStrictEqual for object compare

--- analyzeArr.test.js
+++ analyzeArr.test.js
@@ -1,7 +1,7 @@
 const analyzeArr = require("./analyzeArr");
 
 test("analyze 1 8 3 4 2 6]", () => {
-  expect(analyzeArr([1, 8, 3, 4, 2, 6])).toBe({
+  expect(analyzeArr([1, 8, 3, 4, 2, 6])).toStrictEqual({
     average: 4,
     min: 1,
     max: 8,
@@ -10,7 +10,7 @@ test("analyze 1 8 3 4 2 6]", () => {
 });
 
 test("analyze 1 2 3 4 5 6 7 8 9 10]", () => {
-  expect(analyzeArr([1, 8, 3, 4, 2, 6])).toBe({
+  expect(analyzeArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toStrictEqual({
     average: 5.5,
     min: 1,
     max: 10,
