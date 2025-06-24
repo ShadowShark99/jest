d197996791754ccb6f80761e8691265f4abc4a7c
ShadowShark99 <kadenwchan@gmail.com>
2025-06-23 19:14:59 -0500

    Write analyzeArr tests

---
+++ analyzeArr.test.js
@@ -0,0 +1,19 @@
+const analyzeArr = require("./analyzeArr");
+
+test("analyze 1 8 3 4 2 6]", () => {
+  expect(analyzeArr([1, 8, 3, 4, 2, 6])).toBe({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("analyze 1 2 3 4 5 6 7 8 9 10]", () => {
+  expect(analyzeArr([1, 8, 3, 4, 2, 6])).toBe({
+    average: 5.5,
+    min: 1,
+    max: 10,
+    length: 10,
+  });
+});
